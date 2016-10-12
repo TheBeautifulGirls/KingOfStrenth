29bd59bb055f3e2b6d3bcb4a64ec29cec2ca16ba
cll12_12 <cll12_12@126.com>
2016-10-12 14:27:57 +0800

    消息页面逻辑修改，个人设置中添加注销功能

--- KingOfStrenth/KingOfStrenth/Controller/SettingAndNews/SystemSettingsViewController.swift
+++ KingOfStrenth/KingOfStrenth/Controller/SettingAndNews/SystemSettingsViewController.swift
@@ -10,10 +10,54 @@ import UIKit
 
 class SystemSettingsViewController: UIViewController {
 
+    // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.clearColor()
-
+        
+        initLayout()
+        layoutPageSubViews()
     }
+    
+    // MARK: - private method
+    func initLayout() {
+        self.view.addSubview(logoutBtn)
+        
+    }
+    
+    func layoutPageSubViews() {
+        logoutBtn.snp_makeConstraints { (make) in
+            make.centerX.equalTo(self.view)
+            make.bottom.equalTo(self.view.snp_bottom).offset(-30)
+            make.width.equalTo(80)
+            make.height.equalTo(35)
+        }
+    }
+    
+    func logoutAction(sender: UIButton) {
+        YAlertViewController.showAlertControllerCompleion({
+            
+            let userDetault = NSUserDefaults.standardUserDefaults()
+            userDetault.removeObjectForKey("This is userInfo")
+            userDetault.synchronize()
+            
+            let loginVC = LoginViewController()
+            self.navigationController?.pushViewController(loginVC, animated: false)
+            
+            }, view: self, title: "提示", message: "确定要退出吗？")
+    }
+    
+    // MARK: - setters and getters
+    var _logoutBtn: UIButton!
+    var logoutBtn: UIButton {
+        if _logoutBtn == nil {
+            _logoutBtn = UIButton()
+            _logoutBtn.setBackgroundImage(UIImage(named: "Setting_cancelAccount_btn_normal_iPhone"), forState: .Normal)
+            _logoutBtn.setBackgroundImage(UIImage(named: "Setting_cancelAccount_btn_selected_iPhone"), forState: .Highlighted)
+            _logoutBtn.addTarget(self, action: #selector(SystemSettingsViewController.logoutAction(_:)), forControlEvents: .TouchUpInside)
+        }
+        return _logoutBtn
+    }
+    
 
 }
